2fbf168dbce4637d6b7ac43da23783e96e9c0e05
beingayaz <khanayaz3131@gmail.com>
2019-12-09 18:57:57 +0530

    done

---
+++ src/utils/forecast.js
@@ -0,0 +1,19 @@
+const request = require('request')
+
+
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'https://api.darksky.net/forecast/ba1a3431c0b78f83e6f975de03880889/'+ latitude +','+ longitude
+
+    request({ url, json: true }, (error, { body }) => {
+        if(error) {
+            callback('Unable to connect weather service', undefined)
+        } else if(body.error) {
+            callback('Unable to fin d location', undefined)
+        } else {
+            callback(undefined, body.daily.data[0].summary + ' It is currently '+body.currently.temperature+' degrees out. There is a '+body.currently.precipProbability+ '% chance of rain.');
+        }
+    })
+}
+
+module.exports = forecast
